650deb10a134ba6f42c5571bbdc65bd4923a7988
Sahil Kumar <Sahilkumar32755@gmail.com>
2024-06-27 18:07:17 +0530

    Quick Fixes In Backend.

--- Backend/src/Controllers/Order.controllers.js
+++ Backend/src/Controllers/Order.controllers.js
@@ -14,7 +14,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 */ 
 
 
-const frontendUrl = "https://tomato-food-website-frontend.onrender.com/";
+const frontendUrl = "https://tomato-food-website-frontend.onrender.com";
 
 
 // Placing Order:
